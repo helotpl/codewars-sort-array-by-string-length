f49f3b68ac1b0e5181cc8a6fcab0b9f4feac4dbe
helotpl <barczyk@gmail.com>
2023-12-18 08:35:43 +0100

    my first answer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", sort_by_length(&["wdf".to_string(),"c".to_string(),"bb".to_string(),"xyz".to_string()]));
+}
+
+fn sort_by_length(arr: &[String]) -> Vec<String> {
+    let mut v = Vec::from(arr);
+    v.sort_by(|a, b| a.len().cmp(&b.len()));
+    v
 }
